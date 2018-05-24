f4e52d341e7fdfaa57285103c78ae3fdb1f31511
Simon Turquier <turquiersimon@hotmail.fr>
2018-05-24 11:14:36 +0200

    Setup

---
+++ src/board/components/Board.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="grid">
+		<div class="tile-container">
+			<div class="tile" v-for="n in grid.length" :key="n"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Board from '@/board/board';
+
+export default {
+	name: 'Board',
+	// components: 
+	data() {
+		return {
+			grid: [],
+			gridSize: 16,
+			line: 4
+		}
+	},
+	created() {
+		Board.init(this.gridSize);
+		// this.grid = Board.squares;
+		for (var i = 0; i < this.line; i++) {
+			// debugger;
+			this.grid.push(new Array(this.line));
+			for (var j = 0; j < this.line; j++) {
+				this.grid[i][j] = (i+j);
+			}
+		}
+		console.log(this.grid);
+	}
+}
+
+</script>
+
+<style>
+
+.grid {
+	width: 400px;
+	height: 400px;
+	background: purple;
+}	
+.tile {
+	width: 40px;
+	height: 40px;
+	background: red;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Board from '@/board/components/Board'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Board',
+      component: Board
     }
   ]
 })
