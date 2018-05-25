f684f8990c2100a23aba5641c8d727512f7bc474
Simon Turquier <turquiersimon@hotmail.fr>
2018-05-25 10:45:10 +0200

    Fix little timer bug

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -73,7 +73,7 @@ export default {
 		refresh(){
 			board.init(this.gridSize);
 			this.grid = board.squares;
-			timer.methods.resetTimer();
+			timer.methods.resetTimer(this.$refs.minutes, this.$refs.seconds);
 		}
 	}
 }
